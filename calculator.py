bc8d1b79f5552840819971e21bf0e33dcdd8a168
Efe Tüter <tute6006@gmail.com>
2026-02-27 14:25:45 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# Simple calculator
+# Author: Efe Tüter
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose operation: +, -, *, /")
+op = input("Operation: ")
+
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Cannot divide by zero!")
+else:
+    print("Invalid operation")
